d5be17dd6052379b12440e51811ca798ec0bdb51
Miles Florence <milesflo15@gmail.com>
2016-01-18 16:02:17 -0800

    Corrected heroku connection

--- knexfile.js
+++ knexfile.js
@@ -27,10 +27,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    connection: process.env.DATABASE_URL,
     },
     pool: {
       min: 2,
